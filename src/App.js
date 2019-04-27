ffb71a7d0ddc63b4ce86d37081e0b27d8562dee7
Eduardo Gracez Dusik <42807647+eduardodusik@users.noreply.github.com>
2019-04-27 14:35:31 -0300

    Develop (#6)
    * Feat/tela jogo (#4)
    
    * style: adding pelota
    
    * commit inicial
    
    * Feat/tela jogo (#5)
    
    * style: adding pelota
    
    * commit inicial
    
    * fix: remove test function

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
-import React, { Component, Fragment } from 'react';
-import { Button } from './ui/components'
+import React, { Component } from 'react';
+import AppRouter from './routes'
 import './App.css';
-import { Animal } from './ui/components/animais/animal.component'
 
 class App extends Component {
   render() {
     return (
-      <Fragment>
-        <div className="app">
-          {/* <div className="bola" /> */}
-          <div className="app-title">
-            AUTISM APP
-            <div className="animalzinho">
-              <Animal
-                name='ourico'
-                width={200}
-              />
-            </div>
-
-          </div>
-
-          <div className="menu">
-            <Button title="Jogar"></Button>
-            <Button title="Resultados"></Button>
-          </div>
-        </div>
-      </Fragment>
+      <AppRouter />
+ 
     );
   }
 }
