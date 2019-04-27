2d453ea1142368bd44967c618897057b460d6acd
ACCERA\eduardo.dusik <eduardo.dusik@accera.com.br>
2019-04-27 14:33:56 -0300

    fix: remove test function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
 import AppRouter from './routes'
 import './App.css';
-import { Teste } from './http/dtos/httpclient';
-
-onclick = () => {
-  Teste();
-}
 
 class App extends Component {
   render() {
